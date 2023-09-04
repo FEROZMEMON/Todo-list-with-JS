f1749c8627122a86957865323af56fe86680dce2
FEROZMEMON <ferozmemon726@gmail.com>
2023-09-04 11:10:43 +0500

    styling remaining

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
 let input = document.querySelector(".todo")
 let ol = document.querySelector("ol")
 
+
+
 let todo = []
 
 function addTodo(e) {
     e.preventDefault();
 
-    // ol.innerHTML = ""
+    if (input.value == "") {
+        alert("enter the task")
 
-    todo.push(input.value)
+    }
+    else {
 
-    console.log(todo);
+
+        ol.innerHTML = ""
+
+        todo.push(input.value)
+
+        console.log(todo);
+
+        for (let i = 0; i < todo.length; i++) {
+
+            ol.innerHTML += `<li> ${todo[i]} </li> <button onclick="deleteTodo(${i})" >Delete</button> 
+        <button onclick="editTodo(${i})">Edit</button> `
+
+        }
+
+        input.value = ""
+    }
+}
+function deleteTodo(i) {
+    ol.innerHTML = ""
+    todo.splice(i, 1)
 
     for (let i = 0; i < todo.length; i++) {
 
-        ol.innerHTML += `<li> ${todo[i]} </li> <button onclick="deleteTodo${i}" >Delete</button> 
-        <button onclick="editTodo${i}">Edit</button> `
-       
+        ol.innerHTML += `<li> ${todo[i]} </li> 
+    <button onclick="deleteTodo(${i})" >Delete</button> 
+    <button onclick="editTodo(${i})">Edit</button> `
     }
 
-    input.value = ""
 }
 
-function deleteTodo(i){
+
+function editTodo(i) {
     ol.innerHTML = ""
-    todo.splice(i, 1)
-    ol.innerHTML += `<li> ${todo[i]} </li> <button onclick="deleteTodo${i}" >Delete</button> 
-    <button onclick="editTodo${i}">Edit</button> `
-   
+
+    let user = prompt("Edit the list")
+
+    todo.splice(i, 1, user)
+    for (let i = 0; i < todo.length; i++) {
+
+        ol.innerHTML += `<li> ${todo[i]} </li> 
+     <button onclick="deleteTodo(${i})" >Delete</button> 
+     <button onclick="editTodo(${i})">Edit</button> `
+
+    }
+
 }
 
+console.log(todo);
 
 
 
@@ -44,74 +76,6 @@ function deleteTodo(i){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // function delete() {
-    //     arr.splice(0, 1);
-        // renderTasks();
-    // }
-
-    // function editTask() {
-    //     const newText = prompt('Edit task:', arr[0]);
-    //     if (newText !== null) {
-    //         arr[0] = newText;
-            // renderTasks();
-    //     }
-    // }
-
-    // function renderTasks() {
-    //     ul.innerHTML = "";
-    //     for (let i = 0; i < .length; i++) {
-    //         const list = todo[i];
-    //         ul.innerHTML += `
-    //         <li>${list}
-    //             <button onclick="deleteTask(${i})">Delete</button>
-    //             <button onclick="editTask(${i})">Edit</button>
-    //         </li>`;
-        // }
-    // }
 
 
 
